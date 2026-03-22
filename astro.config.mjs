31a58b30f6e13d5f92bd33d2b28b585f5834577b
peshalniraula <152018417+peshalniraula@users.noreply.github.com>
2026-03-22 12:58:19 +0545

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://corbig.com',
+  site: 'https://peshalniraula.com.np',
   vite: {
     plugins: [tailwindcss()],
   },
